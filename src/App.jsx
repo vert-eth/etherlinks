8bb378f96255eaee375af88f1a7535fe80d8224b
vert.eth <101998556+vert-eth@users.noreply.github.com>
2022-04-11 22:17:52 +0000

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState, require, keys, context, process } from "react";
 import './App.css';
+import imgList from './images';
+import jsonList from './links';
+import logo from '../img/vert-logo-new.png';
 
 function App() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  console.log(queryString);
+  
+  function getArgs() {
+    return queryString.substring(1);
+  }
+  
+  let linkList = [];
+  
+  var imageList = [];
+  for (var l in jsonList) {
+    let link = jsonList[l];
+
+    let newLink = {
+      url: link.link,
+      type: link.type,
+      path: link.path,
+      name: link.text,
+      image: imgList[link.id],
+      show: link.show
+    };
+    linkList.push(newLink);
+  }
+  
   return (
     <main>
-      React⚛️ + Vite⚡ + Replit🌀
+      <div className="dataContainer">
+        <div className="innerContainer">
+          <div className="contentContainer">
+            <img className="bigLogo" src={logo} />
+            <h2>vert.eth</h2>
+            <h4 id="walletText">{getArgs()}</h4>
+            <p>I'm a passionate artist and developer building in the crypto and web3 space. Get connected below!</p>
+          </div>
+          <div className="linkList">
+            {linkList.map((a, i) => {
+              if (a.show) {
+                return (
+                  <a className={"majorLink " + a.type} href={a.url} key={i + "_link"}  target="_blank" rel="noopener noreferrer">
+                    <div className="linkContainer" key={i + "container"}>
+                      <img className="linkIcons" src={a.image} id={a.name} />
+                      {a.name}
+                    </div>
+                  </a>
+                );
+              }
+            })}
+            
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
 
 export default App;
+
+/*
+
+NOTES
+
+*/
---
+++ src/images.js
@@ -0,0 +1,17 @@
+import twitter from '../img/twitter.png';
+import github from '../img/github.png';
+import discord from '../img/discord-3.png';
+import foundation from '../img/foundation.png';
+import opensea from '../img/opensea.png';
+import website from '../img/website.png';
+
+var images = {
+  TWITTER: twitter,
+  GITHUB: github,
+  DISCORD: discord,
+  FOUNDATION: foundation,
+  OPENSEA: opensea,
+  WEBSITE: website
+}
+
+export default images;
